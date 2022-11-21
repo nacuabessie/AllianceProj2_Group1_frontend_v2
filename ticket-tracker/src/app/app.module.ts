3dd4a2214375824af2474772a36d9c1ede1f7264
bessie nacua <bessie.nacua@usjr.edu.ph>
2022-11-21 19:28:17 +0800

    Added Login and Dashboard componentfile

--- ticket-tracker/src/app/app.module.ts
+++ ticket-tracker/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
