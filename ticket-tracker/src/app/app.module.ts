7d5008ca0a0dc22c0b0b2b2fbaf1e919f27dffaf
bessie nacua <bessie.nacua@usjr.edu.ph>
2022-11-21 20:20:10 +0800

    Added component and routing files

--- ticket-tracker/src/app/app.module.ts
+++ ticket-tracker/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DashboardModule
+    DashboardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ticket-tracker/src/app/dashboard/dashboard.module.ts
+++ ticket-tracker/src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { TicketPageComponent } from './ticket-page/ticket-page.component';
+import { ManagementPageComponent } from './management-page/management-page.component';
+import { MonthlyReportPageComponent } from './monthly-report-page/monthly-report-page.component';
 
 
 @NgModule({
   declarations: [
-    DashboardPageComponent
+    DashboardPageComponent,
+    HomePageComponent,
+    TicketPageComponent,
+    ManagementPageComponent,
+    MonthlyReportPageComponent
   ],
   imports: [
     CommonModule,
